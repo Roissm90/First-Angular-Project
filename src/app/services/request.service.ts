d57f01db9e4c88410df6fedd620981a83625c64a
rois90 <roissm90@gmail.com>
2024-02-21 11:14:25 +0100

    env.ts

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API_URL } from '../environment';
+import { API_URL } from '../env';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API_URL } from '../environment';
+import { API_URL } from '../env';
 import { Observable } from 'rxjs';
 
 @Injectable({
